4343477e3b3087cb46440bd1669a37cd07f1150e
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-08-18 22:05:00 +0600

    Simple Bank System 1/4

---
+++ src/CardAnatomy/Main.java
@@ -0,0 +1,8 @@
+package CardAnatomy;
+
+public class Main {
+    public static void main(String[] args) {
+        AccountUser accountUser = new AccountUser();
+        accountUser.startSystem();
+    }
+}
